d06aa0dd26d39a18ff6a82dce6174e93b7f4e010
JosipTito <50152295+JosipTito@users.noreply.github.com>
2019-05-03 13:50:43 -0500

    Add files via upload

---
+++ Apple.java
@@ -0,0 +1,44 @@
+import java.awt.color.*;
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Apple
+{
+	private int xCoord, yCoord, width, height;
+	
+	public Apple(int xC, int yC, int size)
+	{
+		xCoord = xC;
+		yCoord = yC;
+		width = size;
+		height = size;
+	}
+	
+	public void draw(Graphics g)
+	{
+		g.setColor(Color.RED);
+		g.fillOval(xCoord * width, yCoord * height, width, height);
+	}
+	
+	public int getXCoord()
+	{
+		return xCoord;
+	}
+	
+	public void setXCoord(int xC)
+	{
+		xCoord = xC;
+	}
+	
+	public void setYCoord(int yC)
+	{
+		yCoord = yC;
+	}
+	
+	public int getYCoord()
+	{
+		return yCoord;
+	}
+	
+	
+}
