091a73ddf227c1689492016977826a7a62c36e25
JosipTito <50152295+JosipTito@users.noreply.github.com>
2019-05-06 13:57:58 -0500

    Add files via upload

--- Body.java
+++ Body.java
@@ -1,71 +1,45 @@
 import java.awt.color.*;
-
 import java.awt.Color;
-
 import java.awt.Graphics;
 
 public class Body
-
 {
-
 	private int xCoord, yCoord, width, height;
-
+	
 	public Body(int xC, int yC, int size)
-
 	{
-
 		xCoord = xC;
-
 		yCoord = yC;
-
 		width = size;
-
 		height = size;
-
 	}
-
-	public void draw(Graphics g)
+	
+	public void draw(Graphics g) 
 	{
-			g.setColor(Color.GREEN);
-
-			g.fillRect(xCoord * width, yCoord * height, width, height);
-
-			g.setColor(Color.GREEN);
-
-			g.fillRect(xCoord * width + 1, yCoord * height + 1, width - 2, height - 2);
-
+		g.setColor(Color.GREEN);
+		g.fillRect(xCoord * width, yCoord * height, width, height);
+		g.setColor(Color.GREEN);
+		g.fillRect(xCoord * width + 1, yCoord * height + 1, width - 2, height - 2);
 	}
-
 	public int getXCoord()
-
 	{
-
 		return xCoord;
-
 	}
-
+	
 	public void setXCoord(int xC)
-
 	{
-
 		xCoord = xC;
-
 	}
-
+	
 	public void setYCoord(int yC)
-
 	{
-
 		yCoord = yC;
-
 	}
-
+	
 	public int getYCoord()
-
 	{
-
 		return yCoord;
-
 	}
-
+	
+	
 }
--- Frame.java
+++ Frame.java
@@ -2,13 +2,12 @@ import javax.swing.JFrame;
 
 public class Frame {
 
-    public Frame() {
+    public Frame() 
+    {
         JFrame frame = new JFrame();
-        Screen screen = new Screen();
+        SnakeGame screen = new SnakeGame();
         
-        
-        
-        //
+
         frame.add(screen);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("Snake");
@@ -18,10 +17,5 @@ public class Frame {
         frame.setVisible(true);
         
         
-    }
-
-    public static void main(String[] args) 
-    {
-        new Frame();
     }
 }
