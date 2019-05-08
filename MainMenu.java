2ad0860e1c3f2a3b4046634cb17ee123b600d4bb
JoshLao <50214061+JoshLao@users.noreply.github.com>
2019-05-08 13:32:38 -0500

    Update MainMenu.java

--- MainMenu.java
+++ MainMenu.java
@@ -1,71 +1,52 @@
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JButton;
-import javax.swing.JPanel;
-
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-public class MainMenu extends GridLayout {
+import java.awt.BorderLayout;
+import java.awt.Color;
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+public class MainMenu implements ActionListener {
 	private JFrame f;
-	private JPanel p;
-	private JLabel l;
-	private JButton singlePlayer;
-	private JButton multiPlayer;
-	private JButton exit;
+	private JButton singlePlay;
+	private JPanel panel;
 	public MainMenu()
 	{
-		f = new JFrame("Main Menu");
-		setFrame();
-        p = new JPanel();
-        setPanel();
-        
-        singlePlayer = new JButton("Single-Player");
-        multiPlayer = new JButton("Mutli-Player");
-        exit = new JButton("Exit");
-        setButtons();
-        
-        
-        
-        f.setVisible(true);
-	}
-	private void setPanel()
-	{
-		p.setBackground(Color.DARK_GRAY);
-		p.setSize(500,500);
-		p.setVisible(true);
-		f.add(p);
-	}
-	private void setButtons()
-	{
-		singlePlayer.setSize(200,100);
-		singlePlayer.addActionListener(new ActionListener() 
-		{
-			public void actionPerformed(ActionEvent e)
-			{
-				f.add(singlePlayer, new FrameSinglePlay());
-			}
-		});
-		exit.setSize(100,50);
-		exit.addActionListener(new ActionListener() 
-		{
-			public void actionPerformed(ActionEvent e)
-			{
-				f.add(exit, JFrame.EXIT_ON_CLOSE);
-			}
-		});
-		singlePlayer.setVisible(true);
-		exit.setVisible(true);
-		
-	}
-	private void setFrame()
-	{
+		f = new JFrame("Snake");
+		f.setSize(500, 500);
+		f.setResizable(false);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		f.setSize(500,500);
-		f.setTitle("Snake");
-        f.setResizable(false);
-        f.setLocationRelativeTo(null);
+		
+		panel = new JPanel();
+		
+		panel.setSize(200,300);
+		panel.setAlignmentX(200);
+		panel.setAlignmentY(200);
+		panel.setBackground(Color.BLACK);
+		singlePlay = new JButton("Single-Player");
+		singlePlay.setSize(200,100);
+		singlePlay.addActionListener(this);
+		
+		panel.add(singlePlay,BorderLayout.CENTER);
+		f.add(panel,BorderLayout.CENTER);
+		
+		
+		
+		f.setVisible(true);
 	}
+  public void actionPerformed(ActionEvent ae) {
+    String comStr = ae.getActionCommand();
+   if(comStr.equals("Single-Player"))
+   {
+	   new FrameSinglePlay();
+   }
+   else if(comStr.equals("Multi-Player"))
+   {
+	   //new FrameMultiPlay();
+   }
+   
+  }
+  public static void main(String args[]) 
+  {
+    new MainMenu();
+  }
 }
