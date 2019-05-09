69421cafc8f1e0031de8e147e26b18fc88c01f8e
JoshLao <50214061+JoshLao@users.noreply.github.com>
2019-05-09 13:57:43 -0500

    Update FrameMultiPlayer.java

--- FrameMultiPlayer.java
+++ FrameMultiPlayer.java
@@ -1,8 +1,19 @@
+import javax.swing.JFrame;
 
-public class FrameMultiPlayer
-{
-	public FrameMultiPlayer()
-	{
-		
-	}
+public class FrameMultiPlayer {
+
+    public FrameMultiPlayer() 
+    {
+        JFrame frame = new JFrame();
+        SnakeGameMultiPlay screen = new SnakeGameMultiPlay();
+        frame.add(screen);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Snake");
+        frame.setResizable(false);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+        
+        
+    }
 }
